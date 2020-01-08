39d5a0de243dc4a10bd0f8bcd7b94c30faaf4a38
Julio Cesar <julinn_cesar@hotmail.com>
2020-01-07 22:24:45 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+/**
+ * Switch permite que somente uma pagina seja exibida (BrowserRouter permite que mais de um componente seja exibido)
+ *
+ * `exact` é usado para quando o usuario estiver naquela rota mesmo
+ */
